6f0a822cdab2f6af11e4629e3f41fde9bd7b604e
Mina Badrous <minabadrous.vu@gmail.com>
2021-04-10 17:29:31 +0200

    Refactor findLongestSubstring function

--- Sliding Window/findLongestSubstring.js
+++ Sliding Window/findLongestSubstring.js
@@ -1,34 +1,46 @@
 function findLongestSubstring(string){
-    let len = string.length;
-  
-    // 0 and 1 length cases
-    if(len <= 1) return len;
-  
-    // define maxLen, templen, lookup object
-    let maxLen = 1;
-    let tempLen = 1;
-    let lookup = {};
-  
-    // loop string
-    for(let i=0; i<string.length; i++){
-      let char = string[i];
-      // lookup char
-      if(lookup[char]){
-        // if exists compare Length and reset loop with next index
-        if(tempLen > maxLen) maxLen = tempLen-1;
-        i = i - tempLen + 1;
-        tempLen = 0;
-        lookup = {};
+  // grap an index, loop the string after it, storing charachters met, 
+  // untill meeting an existing charachter, compare the temp and max length,
+  // then start with the next index
+
+  // grap an index
+  let index = 0;
+  // have a lookup to store charachters
+  let lookup = {};
+
+  // start a temp and max length
+  let tempLen = 0;
+  let maxLen = 0;
+
+  // loop a substring
+  while(index < string.length){
+    // found existing character
+    if(lookup[string[index]]){
+      
+      // temp len is bigger
+      if(tempLen > maxLen){
+        maxLen = tempLen;
       }
-      else lookup[char] = 1;
-      tempLen++;
+
+      // reset lookup
+      lookup = {};
+      index = index - tempLen + 1;
+      tempLen = 0;
     }
-    // return maxLen
-    return maxLen;
+
+    // didn't find a match
+    else {
+      lookup[string[index]] = 1;
+      tempLen++;
+      index++;
+    }
   }
+
+  return tempLen > maxLen ? tempLen : maxLen;
+}
   
-  console.log(findLongestSubstring('rithmschool'))
-  // console.log(findLongestSubstring('thecatinthehat'))
+  // console.log(findLongestSubstring('rithmschool'))
+  console.log(findLongestSubstring('thecatinthehat'))
   // console.log(findLongestSubstring('thisisawesome'))
   
   
