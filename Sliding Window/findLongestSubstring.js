ceda3e7bf81c8c3473b9548808658c07741860f7
Mina badrous <mina.badrous@dlivr.io>
2021-04-15 09:34:36 +0200

    Refactor findLongestSubstring function

--- Sliding Window/findLongestSubstring.js
+++ Sliding Window/findLongestSubstring.js
@@ -1,35 +1,40 @@
 function findLongestSubstring(string){
     let len = string.length;
+    console.log('len:', len)
   
     // 0 and 1 length cases
     if(len <= 1) return len;
   
     // define maxLen, templen, lookup object
-    let maxLen = 1;
-    let tempLen = 1;
+    let maxLen = 0;
+    let tempLen = 0;
     let lookup = {};
   
     // loop string
-    for(let i=0; i<string.length; i++){
+    for(let i=0; i<len; i++){
+
       let char = string[i];
       // lookup char
       if(lookup[char]){
         // if exists compare Length and reset loop with next index
-        if(tempLen > maxLen) maxLen = tempLen-1;
+        if(tempLen > maxLen){
+          maxLen = tempLen;
+        } 
+
         i = i - tempLen + 1;
         tempLen = 0;
         lookup = {};
       }
-      else lookup[char] = 1;
-      tempLen++;
+      else{
+        lookup[char] = 1;
+        tempLen++;
+      }
     }
     // return maxLen
-    return maxLen;
+    return maxLen < tempLen ? tempLen : maxLen;
   }
   
-  console.log(findLongestSubstring('rithmschool'))
-  // console.log(findLongestSubstring('thecatinthehat'))
+  // console.log(findLongestSubstring('rithmschool')) //7
+  console.log(findLongestSubstring('thecatinthehat')) //7
+  // console.log(findLongestSubstring('longestsubstring')) //8
   // console.log(findLongestSubstring('thisisawesome'))
-  
-  
-  
