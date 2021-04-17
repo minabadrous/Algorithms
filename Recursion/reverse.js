4eaa2b1bf152dfa8ce96c380da65f202b48f2280
Mina Badrous <minabadrous.vu@gmail.com>
2021-04-17 09:21:05 +0200

    Refactor reverse function

--- Recursion/reverse.js
+++ Recursion/reverse.js
@@ -1,6 +1,6 @@
 const reverse = str =>
     str.length === 0
     ? ''
-    : str[str.length - 1].concat(reverse(str.substring(0, str.length - 1)));
+    : reverse(str.substring(1)) + str[0];
 
 reverse('awesome'); //emosewa
