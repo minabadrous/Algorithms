0599efdab601f27e2d5352958b55835e94658c28
Mina Badrous <minabadrous.vu@gmail.com>
2021-04-27 23:11:32 +0200

    Refactor insertionSort Algorithm

--- Sort/insertionSort.js
+++ Sort/insertionSort.js
@@ -1,5 +1,5 @@
 const insertionSort = arr => {
-    for(let i=0; i<arr.length; i++){
+    for(let i=1; i<arr.length; i++){
         let val = arr[i];
         for(let j=i-1; j>=0 && arr[j] > val; j--){
             arr[j+1] = arr[j];
