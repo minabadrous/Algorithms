c65e9e4c11838ad21b5b38f70a6b4d9f9ea3732c
Mina badrous <mina.badrous@dlivr.io>
2021-05-04 16:26:11 +0200

    Add timeConversion Algorithm

---
+++ HackerRank/easy/timeConversion.js
@@ -0,0 +1,16 @@
+function timeConversion(s) {
+    let zone = s.slice(-2);
+    let edit = s.slice(0,2);
+    let hour = Number(edit);
+
+    if( zone === 'AM' && hour === 12 ) edit = '00';
+    else if( zone === 'PM' && hour < 12 ) edit = hour + 12;
+
+    return ( edit + s.slice(2, -2))
+}
+
+console.log(timeConversion('06:40:03AM'))
+
+// 12AM -> 00
+// PM && !== 12 -> +12
+// 
